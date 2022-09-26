ef7551de99400c753cabd6869a1b1396477316be
Shanic0120 <juazapataca@unal.edu.co>
2022-09-26 15:41:50 -0500

    main

--- pregunta_08.sql
+++ pregunta_08.sql
@@ -41,3 +41,4 @@
 --
 --  >>> Escriba su codigo a partir de este punto <<<
 --
+SELECT strftime('%Y',c23) as "YEAR",avg(c21) from tbl2 GROUP BY strftime('%Y',c23);
--- pregunta_11.sql
+++ pregunta_11.sql
@@ -38,3 +38,4 @@
 --
 --  >>> Escriba su codigo a partir de este punto <<<
 -- 
+SELECT strftime('%Y',c14) as YEAR, COUNT(*) as CANT  from tbl1 WHERE YEAR="2018" GROUP BY YEAR;
